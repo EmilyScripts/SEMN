df560f4891d37ce0a32de17f8161c9628f4ec334
Sangita Sunuwar <sangitasunuwar@gmail.com>
2018-08-23 10:37:06 +0100

    add login and list pages

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const path = require("path");
 const router = express.Router();
 const home = require("./home");
-
+const login = require("./login");
 const error = require("./error");
+const list = require("./list");
 
 router.get("/", home.get);
+router.get("/login", login.get);
+router.get("/list", list.get);
 
 module.exports = router;
