c2ad1c82439ee4e7c7087b4a37e10ded5317cb73
EmilyScripts <emily.rever@gmail.com>
2018-08-23 12:59:00 +0100

    profile page skeleton added

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 const home = require("./home");
 const login = require("./login");
 const error = require("./error");
-const list = require("./list");
+const profile = require("./profile");
 const signup = require('./signup');
 
 router.get("/", home.get);
 router.get("/login", login.get);
-router.get("/list", list.get);
+router.get("/profile", profile.get);
 router.get('/signup', signup.get);
 
 module.exports = router;
